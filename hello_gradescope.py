2db7233986adec9afa4b2f43c63bf0a28ee2f75b
justinmillet <justinmillet1@gmail.com>
2023-04-03 20:27:11 -0700

    Initial commit

---
+++ hello_gradescope.py
@@ -0,0 +1,9 @@
+# Name: Justin Millet
+# OSU Email: milletj@oregonstate.edu
+# Course: CS261 - Data Structures
+# Assignment: Assignment 0: Introduction and Enviroment Setup
+# Due Date: April 12th
+# Description: Confirms with Gradescope I am correctly connected
+
+def gradescope_test():
+    return 42
